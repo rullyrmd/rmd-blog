96f13349ef680754fd9a749d092d243d7beb9d2a
Rully Ramadhan <rully.ramadhan@outlook.com>
2022-01-08 01:05:19 +0700

    initial

---
+++ rmd-blog/rmd-blog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace rmd_blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
